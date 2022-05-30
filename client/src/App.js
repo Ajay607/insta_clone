393d951cd27392d2712ecb14f06c7d43bdedf20e
Ajay <ajaypic8962@gmail.com>
2022-05-30 18:37:33 +0530

    front-end-setup

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+
+import Navbar from "./Components/Navbar/Navbar";
+import Profile from "./Components/Profile/Profile";
+import Signup from "./Components/Signup/Signup";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/sign-up" component={Signup} />
+          <Route exact path="/sign-in" component={Login} />
+          <Route exact path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className='nav-bar'>
+            <Router>
+                <nav>
+                    <div className="nav-wrapper">
+                        <Link to="/" className="brand-logo left">Instagram</Link>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li><Link to="/sign-in">Login</Link></li>
+                            <li><Link to="/sign-up">Signup</Link></li>
+                            <li><Link to="/profile">Profile</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </Router>
+        </div>
+    )
+}
+
+export default Navbar
